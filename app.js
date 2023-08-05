fb9488329242fd42076d2ac8e4c509ca54e76031
shmerv <eaudioworks@gmail.com>
2023-08-05 11:07:31 -0700

    Added coordinates from startingPosition and targetPosition

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const placeKnightButton = document.getElementById('placeKnightButton');
 
+let startingPosition = [];
+let targetPosition = [];
+
 function createBoard() {
     const gameBoard = document.getElementById('gameBoard');
     startButton.addEventListener('click', placeKnight);
+    
 
-    for(let row = 0; row < 8; row ++) {
-        for(let col = 0; col < 8; col ++) {
+    for(let row = 8; row >= 1; row --) {
+        for(let col = 1; col < 9; col ++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
-            cell.setAttribute('data-cell','cell')
+            cell.setAttribute('data-cell','cell');
+            cell.dataset.row = row; // Set the row number as data-row attribute
+            cell.dataset.col = col;
             if((row+col) % 2 === 0) {
                 cell.classList.add('white')
             } else {
@@ -19,7 +25,7 @@ function createBoard() {
     }
 }
 function knightMoves() {
-    message.innerHTML = 'Here is the fastest way'
+    message.innerHTML =" Here is the fastest way, from "+startingPosition+" to "+targetPosition
 
 }
 function placeKnight() {
@@ -35,10 +41,15 @@ function placeKnight() {
                 cell.id = 'knightStart';
                 knightStart = true;
                 message.innerHTML = "Now pick the knight's ending position"
+                
+                startingPosition.push(cell.dataset.row);
+                startingPosition.push(cell.dataset.col);
             } else {
                 cell.classList.add('knightEnd');
                 cell.id = 'knightEnd';
                 endKnight = true;
+                targetPosition.push(cell.dataset.row);
+                targetPosition.push(cell.dataset.col);
                 knightMoves();
             }
             
